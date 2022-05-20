7d385157a576df869ce3ccbcc42115cf0547fdef
ozdemir06 <81537530+ozdemir06@users.noreply.github.com>
2022-05-20 16:35:45 +0300

    Add files via upload

---
+++ linkk.js
@@ -0,0 +1,20 @@
+
+    let i=0;
+   
+    let  x = prompt("link giriniz il kısım")
+    let  a = prompt("link son kısım")
+    let  aa = prompt("kaç link")
+    let  aaa
+    console.log(x)
+    
+    
+for (i=1;i<aa;i++)
+{
+    aaa=x+i+a;
+    console.log(aaa)
+    window.open(aaa,"_blank")
+}
+        
+
+
+
